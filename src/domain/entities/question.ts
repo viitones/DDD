2442940eed0ca0dcd41e86c0891dbc7067c2f721
viitones <vitonesmartins@gmail.com>
2025-04-27 00:26:43 -0300

    :memo: Value Object de slug

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,20 +1,24 @@
 import { randomUUID } from "node:crypto";
+import { Slug } from "./value-objects/slug";
 
 interface QuestionProps {
   title: string, 
   authorId: string, 
   content: string
+  slug: Slug
 }
 export class Question {
   public title: string;
   public content: string;
   public id: string;
   public authorId: string
+  public slug: Slug;
 
   constructor(props: QuestionProps, id?: string) {
     this.title = props.title;
     this.content = props.content;
     this.authorId = props.authorId
     this.id = id ?? randomUUID()
+    this.slug = props.slug
   }
 }
